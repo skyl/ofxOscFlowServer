32b96d97464e678aa70e125a186c4ed8f2d5b56a
Skylar Saveland <skylar.saveland@gmail.com>
2016-05-19 22:44:57 -0700

    initial

---
+++ src/ofApp.cpp
@@ -0,0 +1,75 @@
+#include "ofApp.h"
+#include <map>
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	cout << "listening for osc messages on port " << PORT << "\n";
+	receiver.setup(PORT);
+	ofSetWindowTitle("osc receiver");
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	while
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+
+}
